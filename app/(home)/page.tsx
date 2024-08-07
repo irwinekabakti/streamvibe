42d0afe0ee7148d089ba02723f1c889ed818608d
irwinekabakti <irwinekabakti@gmail.com>
2024-08-07 21:38:34 +0700

    add:(assets & mock data)

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import Image from "next/image";
+
+const Home: FC = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ app/shows/page.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+const page: FC = () => {
+  return (
+    <div>
+      <h1>Shows Page</h1>
+    </div>
+  );
+};
+
+export default page;
